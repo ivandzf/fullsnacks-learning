0b34906c408fbac66ef73f8b849da7ccf02a3328
Divananda Zikry Fadilla <divananda@ruangguru.com>
2022-04-10 13:24:44 +0700

    update method

--- data-structures/rust/src/bin/singly_linked_list.rs
+++ data-structures/rust/src/bin/singly_linked_list.rs
@@ -3,7 +3,7 @@ use std::{
     usize,
 };
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Node<T> {
     value: T,
     next: Option<Box<Node<T>>>,
@@ -55,12 +55,13 @@ impl<T: Debug> LinkedList<T> {
     }
 
     // need traverse operation to get the tail, O(n)
-    fn push(&mut self, v: T) {
+    fn insert_last(&mut self, v: T) {
+        let new_node = Node::new(v);
         match self.head {
-            None => self.head = Some(Node::new(v)),
+            None => self.head = Some(new_node),
             Some(_) => {
-                let last_node = self.get_nth_node(self.count - 1);
-                last_node.map(|node| node.next = Some(Box::new(Node::new(v))));
+                self.get_next_nth_node(self.count - 1)
+                    .map(|node| node.next = Some(Box::new(new_node)));
             }
         }
 
@@ -68,18 +69,51 @@ impl<T: Debug> LinkedList<T> {
         self.count = self.count + 1
     }
 
-    fn pop(&mut self) {
+    // directly set on the head, O(1)
+    fn insert_first(&mut self, v: T) {
+        let mut new_node = Node::new(v);
         match self.head {
+            None => self.head = Some(new_node),
+            Some(_) => {
+                let current_head = self.head.take().unwrap();
+                new_node.next = Some(Box::new(current_head));
+                self.head = Some(new_node);
+            }
+        }
+
+        // manually count, to reduce calculate the lenght of linked list
+        self.count = self.count + 1
+    }
+
+    // need traverse operation to get the tail, O(n)
+    fn remove_last(&mut self) {
+        match &self.head {
             None => {}
             Some(_) => {
-                let mut last_node = self.get_nth_node(self.count - 1);
-                last_node.take();
+                if self.count == 1 {
+                    self.head = None;
+                } else {
+                    self.get_next_nth_node(self.count - 2)
+                        .map(|node| node.next = None);
+                }
+                // self.get_next_nth_node(self.count - 1).take();
+                // manually count, to reduce calculate the lenght of linked list
+                self.count = self.count - 1
             }
         }
     }
 
+    // directly take on the head, O(1)
+    fn remove_first(&mut self) {
+        self.head
+            .take()
+            .map(|head| self.head = head.next.map(|node| *node));
+        // manually count, to reduce calculate the lenght of linked list
+        self.count = self.count - 1
+    }
+
     // traverse and get the linked list by number of index
-    fn get_nth_node(&mut self, n: usize) -> Option<&mut Node<T>> {
+    fn get_next_nth_node(&mut self, n: usize) -> Option<&mut Node<T>> {
         let mut nth_node = self.head.as_mut();
         for _ in 0..n {
             nth_node = match nth_node {
@@ -97,23 +131,31 @@ impl<T: Debug> LinkedList<T> {
             return;
         }
 
-        println!("{:?}", self.head.as_ref().unwrap());
+        println!(
+            "Current length: {}, Linked List: {:?}",
+            self.count,
+            self.head.as_ref().unwrap()
+        );
     }
 }
 
 fn main() {
     let mut ll = LinkedList::new();
-    ll.push(1);
-    ll.push(2);
-    ll.push(5);
+    ll.insert_last(1);
+    ll.insert_last(2);
+    ll.insert_last(5);
 
     ll.display();
 
-    ll.pop();
+    ll.insert_first(20);
 
     ll.display();
 
-    ll.push(10);
+    ll.remove_first();
+
+    ll.display();
+
+    ll.remove_last();
 
     ll.display();
 }
